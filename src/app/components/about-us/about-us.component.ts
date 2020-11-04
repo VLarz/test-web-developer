67aaa9fc243e37947bc29d9df261facce317a81f
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-11-04 21:36:18 +0800

    Finished the about us content

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Content } from '../../models/content.model';
+import { ContentService } from '../../services/content.service';
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
+  data: Content;
 
-  constructor() { }
+  constructor(private contentService: ContentService) { }
 
   ngOnInit(): void {
+    this.getAboutUs();
   }
+
+  getAboutUs(): void {
+    this.contentService.getContent().subscribe(
+      res => this.data = res.find(x => x.id === 'about_us')
+    );
+  }
+
 
 }
