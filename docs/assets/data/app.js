17b111ea804d8fb99865159ec276927ce863f614
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-11-05 15:58:00 +0800

    already built to access on github pages

---
+++ docs/assets/data/app.js
@@ -0,0 +1,19 @@
+var images = {
+	man: {
+		heading: 'Man Image',
+		description: 'Image of a man.',
+		path: 'assets/images/man.jpg'
+	},
+	wizard: {
+		heading: 'Wizard Image',
+		description: 'Image of a wizard.',
+		path: 'assets/images/wizard.jpg'
+	},
+	beast: {
+		heading: 'Beast Image',
+		description: 'Image of a beast.',
+		path: 'assets/images/beast.jpg'
+	}
+};
+
+module.exports = images;
